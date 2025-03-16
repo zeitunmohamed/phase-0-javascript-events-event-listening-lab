83e282af4b11f2ab1ea7312ac69d1e14be35fa88
zeitunmohamed <zeitunmohamed@gmail.com>
2025-03-16 22:19:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.getElementById('button'); // Grabbing the button element
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
 }
+
